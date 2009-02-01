5395c2069c63c2cb1705f824efedf840c5dc73e5
Andrey Somov <py4fun@gmail.com>
2009-02-01 14:07:50 +0100

    use form

--- instantyaml/yamler.py
+++ instantyaml/yamler.py
@@ -1,7 +1,9 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-import os
 from google.appengine.ext.webapp import template
+import os
+import cgi
+import yaml
 
 """
 class Attempt(db.Model):
@@ -16,9 +18,18 @@ class MainPage(webapp.RequestHandler):
     path = os.path.join(os.path.dirname(__file__), 'welcome.html')
     template_values = {}
     self.response.out.write(template.render(path, template_values))
+    
+class ValidatePage(webapp.RequestHandler):
+  def post(self):
+    content = self.request.get('content')
+    path = os.path.join(os.path.dirname(__file__), 'validate.html')
+    template_values = {"validated": content}
+    self.response.out.write(template.render(path, template_values))
+
 
 application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+                                      [('/', MainPage),
+                                      ('/validate', ValidatePage)],
                                      debug=True)
 
 def main():
