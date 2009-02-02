6883069ba5a8bf55f212265137d6a2de268fa970
somov <somov@localhost>
2009-02-03 00:39:07 +0100

    show statistics report

--- instantyaml/admin.py
+++ instantyaml/admin.py
@@ -8,11 +8,35 @@ import cgi
 import yaml
 from yamler import Attempt
 
+class Stat(object):
+    def __init__(self, user, date):
+        self.user = user
+        self.counter = 1
+        self.date = date
+        
+    def add(self, date):
+        self.counter += 1
+        self.date = date
+        
+    
 class AdminPage(webapp.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'admin.html')
+        attempts = {}
         counter = Attempt.all().count()
-        template_values = {"counter": counter, "attempts": Attempt.all()}
+        for attempt in Attempt.all():
+            author = attempt.author.email()
+            if not author:
+                author = "Anonymous@nowhere.com"
+            if author not in attempts:
+                stat = Stat(author, attempt.date)
+                attempts[author] = stat
+            else:
+                stat = attempts[author]
+                stat.add(attempt.date)
+        statistics = attempts.values()
+            
+        template_values = {"counter": counter, "statistics": statistics}
         self.response.out.write(template.render(path, template_values))
         
 application = webapp.WSGIApplication(
